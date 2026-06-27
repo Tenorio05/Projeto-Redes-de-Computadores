ea1510d2b0f3a67a52f21bcdc45deaa9a904bfe2
TheoWill-25 <twrf@cin.ufpe.br>
2026-06-26 22:21:24 -0300

    Update Print.py
    codigo agr funcional

--- Terceira_Entrega/Servidor_L/Print.py
+++ Terceira_Entrega/Servidor_L/Print.py
@@ -1,20 +1,16 @@
+from rich.console import Console
+from rich.live import Live
+from rich.panel import Panel
 
+console = Console()
 
-def msg(mensagem):
-    # Descobre quantas quebras de linha existem no texto atual
-    linhas = mensagem.count('\n')
-    
-    # Se já tivermos impresso algo antes, esse código faz o cursor 
-    # subir de volta o número exato de linhas para reescrever por cima
-    if hasattr(msg, "ja_imprimiu") and msg.ja_imprimiu:
-        # \033[F sobe uma linha. Multiplicamos pelo número de linhas do texto anterior.
-        print(f"\033[{msg.linhas_anteriores}F", end="")
-    
-    # Imprime o novo texto preenchendo os espaços para não deixar rastro
-    # (Dividimos o texto para aplicar o preenchimento de espaços em cada linha dele)
-    texto_formatado = "\n".join([f"{linha: <120}" for linha in mensagem.split("\n")])
-    print(texto_formatado, end="", flush=True)
-    
-    # Guarda na memória da função para a próxima vez que ela for chamada
-    msg.ja_imprimiu = True
-    msg.linhas_anteriores = linhas
+live = Live(refresh_per_second=10)
+live.start()
+
+def msg(texto):
+    live.update(
+        Panel(
+            texto,
+            title="Sistema de Leilão"
+        )
+    )
