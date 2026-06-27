6663aa27ead3e114e315077528f9d3f22bb5c4eb
TheoWill-25 <twrf@cin.ufpe.br>
2026-06-26 22:50:21 -0300

    Update Print.py
    comentado

--- Terceira_Entrega/Servidor_L/Print.py
+++ Terceira_Entrega/Servidor_L/Print.py
@@ -2,12 +2,19 @@ from rich.console import Console
 from rich.live import Live
 from rich.panel import Panel
 
+# Instancia o console da biblioteca "rich" (usada para textos bonitos no terminal)
 console = Console()
 
+# Inicia um bloco "Live". Isso permite que o texto na tela do servidor
+# seja atualizado dinamicamente na mesma posição, sem "printar" várias linhas para baixo
 live = Live(refresh_per_second=10)
 live.start()
 
 def msg(texto):
+    """
+    Função chamada pelo PC_Leilao para atualizar o que está escrito no painel.
+    O texto passado será encapsulado dentro de um quadro decorado com o título 'Sistema de Leilão'.
+    """
     live.update(
         Panel(
             texto,
